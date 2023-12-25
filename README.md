82ee800633791f54b5cd42aeb19dc1e06c9fc3eb
Tetraslam <bhowmickshresht@gmail.com>
2023-12-25 22:42:57 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# TetraCLI
+
+## Purpose
+This is a small hobby project I started to create a direct communication line between anyone who wants to reach me on github. I'm also using this to practice my python and firebase skills.
+
+
+## Installation
+Planned release on PyPI
+
+
+
+## Features
+- [x] Browse my projects
+- [x] Learn a bit about me
+- [ ] Real-time chat
+- [ ] Missed mail chats if I'm offline
+- [ ] My nvim config
+- [ ] Listen to my last played song on Spotify
+- [ ] Read my blog posts
+- [ ] Check out my favorite content at the moment (blogs, movies & TV, videogames, books, anime, kdramas, etc.)
+- [ ] Coding challenge of the day
---
+++ main.py
@@ -0,0 +1,86 @@
+import os
+import sys
+import firebase_admin
+from dotenv import load_dotenv
+from firebase_admin import credentials, firestore
+from rebullet import Bullet, Input, Numbers, VerticalPrompt, YesNo, SlidePrompt, colors
+from rich import print
+
+load_dotenv()
+
+cred = credentials.Certificate(os.getenv("private_key"))
+app = firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+def add_document(collection: str, document_name: str, info: dict):
+  doc_ref = db.collection(collection).document(document_name)
+  doc_ref.set(info)
+
+
+def add_project_to_firebase():
+  add_project = VerticalPrompt(
+    [
+      Input("Project name?"),
+      Bullet("Main language?",
+            choices = ["C++", "Python", "Javascript", "Go", "C#", "Mojo", "Not here!"]),
+      Input("Give the project a short description! "),
+      ],
+      spacing = 1
+    )
+  add_project.launch()
+  add_document("projects", add_project.result[0][1], {"name": add_project.result[0][1], "language": add_project.result[1][1], "description": add_project.result[2][1]})
+
+def read_projects_from_firebase():
+  projects = []
+  projects_firebase = db.collection("projects")
+  projects_list = projects_firebase.stream()
+  for doc in projects_list:
+    projects.append(doc.to_dict())
+  for project in projects:
+    print(f"\n[purple][bold]Project name: {project['name']}[/bold]\nLanguage: {project['language']}\nDescription: {project['description']}\n[/purple]")
+
+
+def read_profile():
+  profile_collection = db.collection("profile")
+  profile_stream = profile_collection.stream()
+  info = []
+  for doc in profile_stream:
+    if doc.id == "profile":
+      info.append(doc.to_dict())
+  for element in info:
+    print(f"\n[bold purple]About me:[/bold purple] [cyan]{element['bio']}[/cyan]")
+    for i in element['favorites']:
+      print(f"[bold purple]Favorite {i}:[/bold purple] [cyan]{element['favorites'][i]}[/cyan]")
+
+def exit_main_menu():
+  sys.exit(0)
+
+def main_menu():
+  bright_purple = colors.background["magenta"]
+  bright_cyan = colors.bright(colors.foreground["cyan"])
+  menu = Bullet("Welcome to TetraCLI! What would you like to do today?",
+             choices = ["Add a project", "Browse projects", "Look at Tetraslam's profile", "Exit TetraCLI"],
+             word_color = bright_cyan,
+             word_on_switch = bright_cyan,
+             background_on_switch = bright_purple)
+  
+  choice = menu.launch()
+  
+  if choice == "Add a project":
+    add_project_to_firebase()
+    main_menu()
+  elif choice == "Browse projects":
+    read_projects_from_firebase()
+    main_menu()
+  elif choice == "Look at Tetraslam's profile":
+    read_profile()
+    main_menu()
+  elif choice == "Exit TetraCLI":
+    exit_main_menu()
+    
+  
+if __name__ == "__main__":
+  main_menu()
+
+# print(project_name.result[1][1]) THIS GIVES THE RESULT
